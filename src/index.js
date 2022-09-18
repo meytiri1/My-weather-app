566d6e4f839a259c8a7f16a4aab92bcbdb59a05f
Michelle K <109901620+meytiri1@users.noreply.github.com>
2022-09-18 13:38:34 +0100

    Final adjustment of bootstrap layout for small screens

--- src/index.js
+++ src/index.js
@@ -95,7 +95,7 @@ function displayForecast(response) {
 
   let forecastHTML = `<div class="row justify-content-center">`;
   forecast.forEach(function (forecastDay, index) {
-    if (index < 5) {
+    if (index > 0 && index < 6) {
       forecastHTML =
         forecastHTML +
         `<div class="col-2 text-center weather-fore">
